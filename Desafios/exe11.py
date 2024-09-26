6f0717a5c961c3236bec20e4dbaa6cf87a1421ea
David Maia <david.goncalves@dcx.ufpb.br>
2024-09-26 11:19:44 -0300

    Revert "Exercícios 11, 12 e 13"
    This reverts commit b632bf386b1ce332589c4bc2d78f18a66d3aafa9.

--- Desafios/exe11.py
+++
@@ -1,5 +0,0 @@
-#Use a função range() em um "for loop" para 
-#imprimir os número de 1 a 10 na tela.
-
-for i in range(1, 11, 1):
-    print(i)
--- Desafios/exe13.py
+++
@@ -1,9 +0,0 @@
-#Use um loop (que não seja o for) para imprimir
-#os números de 1 a 10 na tela.
-
-num = 10
-contador = 0
-
-while contador < num:
-    contador += 1
-    print(contador)
